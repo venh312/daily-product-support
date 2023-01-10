0573ffa47ed03fa59ea254d3107d771a0eea3aca
venh <conf312@naver.com>
2023-01-10 17:38:51 +0900

    initialize project

---
+++ src/main/java/com/daily/product/support/DailyProductSupportApplication.java
@@ -0,0 +1,13 @@
+package com.daily.product.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyProductSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyProductSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daily/product/support/DailyProductSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daily.product.support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyProductSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
